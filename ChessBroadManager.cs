6041cbaf35e51626cbb22b8b91b223522a29d4d4
HuyMasterTX <huymastertx88@gmail.com>
2024-04-17 19:40:53 +0700

    fix protection level protected -> public

--- ChessBroadManager.cs
+++ ChessBroadManager.cs
@@ -1,57 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace xO {
 
-namespace xO
-{
-    public class ChessBroadManager
-    {
-        #region Properties
-        private Panel chessBroad;
+	public class ChessBroadManager {
 
-        public Panel ChessBroad
-        {
-            get { return chessBroad; }
-            set { chessBroad = value; }
-        }
-        #endregion
+		#region Properties
 
-        #region Intitialize
-        public ChessBroadManager(Panel chessBoard)
-        {
-            this.ChessBroad = chessBoard;
-        }
-        #endregion
+		private Panel chessBroad;
 
-        #region Methods
-        void DrawChessBoard()
-        {
-            Button oldButton = new Button() { Width = 0, Location = new Point(0, 0) };
-            for (int i = 0; i < Cons.CHESS_BOARD_HEIGHT; i++)
-            {
-                for (int y = 0; y < Cons.CHESS_BOARD_WIDTH; y++)
-                {
-                    Button btn = new Button()
-                    {
-                        Width = Cons.CHESS_WIDTH,
-                        Height = Cons.CHESS_HEIGHT,
-                        Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y)
-                    };
+		public Panel ChessBroad {
+			get { return chessBroad; }
+			set { chessBroad = value; }
+		}
 
-                    ChessBroad.Controls.Add(btn);
+		#endregion Properties
 
-                    oldButton = btn;
-                }
-                oldButton.Location = new Point(0, oldButton.Location.Y + Cons.CHESS_WIDTH);
-                oldButton.Width = 0;
-                oldButton.Height = 0;
-            }
-        }
+		#region Intitialize
 
-        #endregion
+		public ChessBroadManager(Panel chessBoard) {
+			ChessBroad = chessBoard;
+		}
 
+		#endregion Intitialize
 
-    }
+		#region Methods
+
+		public void DrawChessBoard() {
+			Button oldButton = new() { Width = 0, Location = new Point(0, 0) };
+			for (int i = 0; i < Cons.CHESS_BOARD_HEIGHT; i++) {
+				for (int y = 0; y < Cons.CHESS_BOARD_WIDTH; y++) {
+					Button btn = new() {
+						Width = Cons.CHESS_WIDTH,
+						Height = Cons.CHESS_HEIGHT,
+						Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y)
+					};
+
+					ChessBroad.Controls.Add(btn);
+
+					oldButton = btn;
+				}
+				oldButton.Location = new Point(0, oldButton.Location.Y + Cons.CHESS_WIDTH);
+				oldButton.Width = 0;
+				oldButton.Height = 0;
+			}
+		}
+
+		#endregion Methods
+	}
 }
